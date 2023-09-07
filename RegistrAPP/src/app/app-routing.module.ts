f4d47a5923e1c9fbeaaec10b0ef1634faaefb34d
Dieghost777 <die.gallardo@duocuc.cl>
2023-09-07 20:15:40 -0300

    Arreglo

--- RegistrAPP/src/app/app-routing.module.ts
+++ RegistrAPP/src/app/app-routing.module.ts
@@ -10,7 +10,8 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },  {
+  },
+  {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
@@ -19,13 +20,8 @@ const routes: Routes = [
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
   {
-<<<<<<< HEAD
     path: 'forpass',
     loadChildren: () => import('./forpass/forpass.module').then( m => m.ForpassPageModule)
-=======
-    path: 'datos-usuario',
-    loadChildren: () => import('./datos-usuario/datos-usuario.module').then( m => m.DatosUsuarioPageModule)
->>>>>>> 209b5ff0d9d3015c6c7f8169023ec140427b7c7a
   },
 
 ];
