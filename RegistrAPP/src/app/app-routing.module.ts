343a9b9b247048ffcd4b6dac0816733ceb6f7722
NazaBkn <Naz.gonzalez@duocuc.cl>
2023-09-07 20:00:57 -0300

    Primer restablecer contraseña

--- RegistrAPP/src/app/app-routing.module.ts
+++ RegistrAPP/src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: 'forpass',
+    loadChildren: () => import('./forpass/forpass.module').then( m => m.ForpassPageModule)
+  },
 
 ];
 
---
+++ RegistrAPP/src/app/forpass/forpass.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forpass',
+  templateUrl: './forpass.page.html',
+  styleUrls: ['./forpass.page.scss'],
+})
+export class ForpassPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
