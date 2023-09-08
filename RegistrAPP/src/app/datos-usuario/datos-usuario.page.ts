a3a7b15f3a53ab38db37f1735135c209c4cbedbc
Dieghost777 <die.gallardo@duocuc.cl>
2023-09-08 01:14:21 -0300

    ULTIMO COMMIT ESTAMOS LISTOS

--- RegistrAPP/src/app/datos-usuario/datos-usuario.page.ts
+++ RegistrAPP/src/app/datos-usuario/datos-usuario.page.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-datos-usuario',
   templateUrl: './datos-usuario.page.html',
   styleUrls: ['./datos-usuario.page.scss'],
 })
-export class DatosUsuarioPage implements OnInit {
+export class DatosUsuarioPage {
+  usuario: any = {}; 
 
-  constructor() { }
+  constructor(private location: Location, private alertController: AlertController) {
+    const usuarioString = localStorage.getItem('usuario');
 
-  ngOnInit() {
+    if (usuarioString) {
+      this.usuario = JSON.parse(usuarioString);
+    }
   }
-  
 
+  goBack() {
+    this.location.back();
+  }
 }
