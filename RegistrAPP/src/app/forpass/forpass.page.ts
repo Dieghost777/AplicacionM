efbeed43bff7ef37db02306912a7e08b503fa137
NazaBkn <Naz.gonzalez@duocuc.cl>
2023-09-08 01:52:43 -0300

    op

--- RegistrAPP/src/app/forpass/forpass.page.ts
+++ RegistrAPP/src/app/forpass/forpass.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 
 @Component({
@@ -6,12 +6,28 @@ import { Location } from '@angular/common';
   templateUrl: './forpass.page.html',
   styleUrls: ['./forpass.page.scss'],
 })
-export class ForpassPage implements OnInit {
+export class ForpassPage {
+  correo!: string; // Marcar como "definitivamente asignada"
 
-  constructor(private location: Location) { }
+  constructor(private location: Location) {}
 
-  ngOnInit() {
+  verificarCorreo() {
+    // Obtener la lista de usuarios registrados del localStorage
+    const usuariosRegistradosString = localStorage.getItem('usuarios');
+    const usuariosRegistrados = usuariosRegistradosString ? JSON.parse(usuariosRegistradosString) : {};
+
+    // Verificar si el correo ingresado existe en la lista de usuarios
+    if (usuariosRegistrados[this.correo]) {
+      // El correo existe en la lista de usuarios registrados
+      console.log('Correo registrado');
+      // Aquí puedes tomar medidas como enviar un correo de restablecimiento de contraseña
+    } else {
+      // El correo no existe en la lista de usuarios registrados
+      console.log('Correo no registrado');
+      // Aquí puedes mostrar un mensaje al usuario indicando que el correo no está registrado
+    }
   }
+
   goBack() {
     this.location.back();
   }
