fac72dce86b45109cf41e34ce077e8cbdc30e5dd
Dieghost777 <die.gallardo@duocuc.cl>
2023-09-08 02:12:18 -0300

    CAMBIO y confirmacion

--- RegistrAPP/src/app/register/register.page.ts
+++ RegistrAPP/src/app/register/register.page.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
-  formularioRegistro!: FormGroup; 
+export class RegisterPage {
+  formularioRegistro!: FormGroup;
 
-  constructor(private location: Location, private alertController: AlertController) {}
+  constructor(
+    private location: Location,
+    private alertController: AlertController,
+    private toastController: ToastController,
+    private fb: FormBuilder // Agrega el FormBuilder aquí
+
+  ) {}
 
   ngOnInit(): void {
     this.formularioRegistro = new FormGroup({
@@ -22,24 +29,39 @@ export class RegisterPage implements OnInit {
   }
 
   async guardar() {
-    var f= this.formularioRegistro.value;
+    var f = this.formularioRegistro.value;
     if (this.formularioRegistro.invalid) {
       const alert = await this.alertController.create({
-        message: 'Porfavor Rellena correctamente el Formulario.',
+        message: 'Por favor, completa correctamente el formulario.',
         buttons: ['OK'],
       });
 
       await alert.present();
-      return
+      return;
     }
+
     var usuario = {
       nombre: f.nombre,
       correo: f.correo,
-      contraseña: f.contraseña
-    }
-    localStorage.setItem('usuario',JSON.stringify(usuario));
+      contraseña: f.contraseña,
+    };
+
+    // Guardar en localStorage
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+
+    // Mostrar mensaje emergente de éxito
+    const toast = await this.toastController.create({
+      message: 'Usuario creado correctamente',
+      duration: 2000, // Duración del mensaje emergente en milisegundos
+      position: 'top', // Puedes ajustar la posición según tus preferencias
+    });
+
+    await toast.present();
+
+    // Limpiar los campos del formulario
+    this.formularioRegistro.reset();
   }
-    
+
   goBack() {
     this.location.back();
   }
